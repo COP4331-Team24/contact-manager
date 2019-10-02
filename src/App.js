520236fd8048e0f53b2b54283adf2f9bdb287ed0
Samantha Stoffel <ahosaral@gmail.com>
2019-10-02 14:30:23 -0400

    Heroku please

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import CreateContact from "./components/CreateContact";
 import Login from "./components/Login";
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:4000/api';
+axios.defaults.baseURL = 'https://contact-manager-group24.herokuapp.com/api';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('auth_token');
 
 function App() {
